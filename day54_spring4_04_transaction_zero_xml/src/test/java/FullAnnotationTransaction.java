e225723e2a46813bcda0d0e19832694e34199c69
miaochangjian <miaochangjian@126.com>
2018-11-15 20:57:30 +0800

    增加4

--- day54_spring4_04_transaction_zero_xml/src/test/java/FullAnnotationTransaction.java
+++ day54_spring4_04_transaction_zero_xml/src/test/java/FullAnnotationTransaction.java
@@ -22,5 +22,6 @@ public class FullAnnotationTransaction {
         account.setUid(45);
         account.setMoney(10000);
         accountService.save(account);
+		System.out.println("=====4444444444=======");
     }
 }
